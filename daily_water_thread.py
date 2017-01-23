589e75e0a2890658b1385be302cf8945c87bdd78
Keaton <linux.keaton@gmail.com>
2017-01-23 06:51:03 -0600

    Retry if no internet
    Just so that if the internet is down at 12:01, the bot doesn't give up on the whole day. For CONTINGENCY_PLAN, I'd suggest emailing/texting/messaging yourself somehow if internet exists. If internet doesn't exist, maybe just keep retrying? Who knows, you'd have a better idea of things to do than I would.

--- daily_water_thread.py
+++ daily_water_thread.py
@@ -10,8 +10,37 @@ import post_templates as posts
 REDDIT_USERNAME = 'takecareofmyplant'
 REDDIT_PASSWORD = 'hunter2'
 
+# Either False or an integer
+LIMIT_RETRIES = 18
+# If LIMIT_RETRIES, the commands to execute
+# Note: this example sucks as a contingency plan 
+CONTINGENCY_PLAN = (
+"""try:
+    q = c.checkKillSwitch()
+    print("Internet exists, must be a Reddit issue.")
+except Exception as e:
+    print("No internet connection exists.\nDetails:\n"+str(e.args))
+""")
+
 # Set-up
-r = c.getReddit()
+
+# Check for internet
+from time import sleep
+count = 0
+while True:
+    try:
+        r = c.getReddit()
+        break
+    except Exception as e:
+        print("An error occurred connecting to Reddit. Trying again in 10 minutes.\nDetails:\n"+str(e.args))
+        sleep(600)
+        if count < LIMIT_RETRIES:
+            count += 1
+        elif LIMIT_RETRIES:
+            exec(CONTINGENCY_PLAN)
+            break
+del sleep
+
 sr = c.getSubReddit(r)
 
 post_body = posts.body
